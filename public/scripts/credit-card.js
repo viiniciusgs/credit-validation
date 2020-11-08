fd461d88c8c3bd71b384e95728bf78e404853fd2
Vinicius Gabriel <viiniciusgs03@gmail.com>
2020-11-08 00:40:01 -0300

    Funcionalidades e estilizaçao nas novas paginas

--- public/scripts/credit-card.js
+++ public/scripts/credit-card.js
@@ -1,8 +1,36 @@
 mainElement = document.querySelector('#main');
 
 const getCard = localStorage.getItem("Card");
-const elementCard = document.createElement('h1');
-const textCard = document.createTextNode(getCard);
+const elementResult = document.createElement('div');
+const elementCard = document.createElement('h2');
+const textCard = document.createTextNode(`Cartão ${getCard}`);
+const elementValid = document.createElement('h1');
+const textValid = document.createTextNode('VÁLIDO');
+
+let imageCard = "";
 
 elementCard.appendChild(textCard);
-mainElement.appendChild(elementCard);
+elementValid.appendChild(textValid);
+elementResult.appendChild(elementCard);
+elementResult.appendChild(elementValid);
+
+mainElement.appendChild(elementResult);
+
+switch(getCard) {
+    case "American Express":
+        imageCard = "/public/images/american-express.svg"
+        break;
+    
+    case "Mastercard":
+        imageCard = "/public/images/master-card.svg"
+        break;
+
+    default:
+        imageCard = "/public/images/visa.svg"
+        break;
+}
+
+const elementImageCard = document.createElement('img');
+elementImageCard.setAttribute('src', imageCard);
+
+mainElement.appendChild(elementImageCard);
