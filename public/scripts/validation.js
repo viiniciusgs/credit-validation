af768e4114f659798cb56c6ca8c1284fb9e52812
Vinicius Gabriel <viiniciusgs03@gmail.com>
2020-11-14 23:51:42 -0300

    Refatorando codigo da pagina principal

--- public/scripts/validation.js
+++ public/scripts/validation.js
@@ -1,5 +1,5 @@
 formElement = document.querySelector('#searchCard');
-inputElement = document.querySelector('#numberCard');
+inputElement = document.querySelector('#cardNumber');
 buttonElement = document.querySelector('#buttonSearchCard');
 
 formElement.onsubmit = function(event) {
@@ -12,82 +12,78 @@ buttonElement.onclick = function() {
 }
 
 function validationCredit() {
-    const creditCard = inputElement.value;
-    var initialNumber = creditCard;
-    var repeatI;
-    var repeatJ;
+    const creditCardNumber = inputElement.value;
+    let repeatI;
+    let repeatJ;
 
-    if(!creditCard) {
+    if(!creditCardNumber) {
         alert('Digite um número de cartão'); 
         inputElement.value = '';
         return;
     }
 
-    while(initialNumber >= 99) {
-        initialNumber  /= 10;
-    }
+    if((creditCardNumber >= 340000000000000 && creditCardNumber < 350000000000000) || 
+    (creditCardNumber >= 370000000000000 && creditCardNumber < 380000000000000)) {
+        repeatI = 100000000000000;
+        repeatJ = 1000000000000000;
 
-    if(Math.trunc(initialNumber) == 34 || Math.trunc(initialNumber) == 37) {
-        var repeatI = 100000000000000;
-        var repeatJ = 1000000000000000;
-
-        luhnsAlgorithm(creditCard, repeatI, repeatJ);
+        luhnsAlgorithm(creditCardNumber, repeatI, repeatJ);
 
         localStorage.setItem('Card', JSON.stringify({
             nameCard: 'American Express',
-            numberCard: creditCard.toString()
+            cardNumber: creditCardNumber.toString()
         }));
     }
 
-    else if(Math.trunc(initialNumber) >= 51 && Math.trunc(initialNumber) <= 55) {
-        var repeatI = 10000000000000000;
-        var repeatJ = 1000000000000000;
+    else if((creditCardNumber >= 5100000000000000 && creditCardNumber < 5600000000000000 ) ||
+    (creditCardNumber >= 2200000000000000 && creditCardNumber < 2300000000000000)) {
+        repeatI = 10000000000000000;
+        repeatJ = 1000000000000000;
         
-        luhnsAlgorithm(creditCard, repeatI, repeatJ);
+        luhnsAlgorithm(creditCardNumber, repeatI, repeatJ);
 
         localStorage.setItem('Card', JSON.stringify({
             nameCard: 'Mastercard',
-            numberCard: creditCard.toString()
+            cardNumber: creditCardNumber.toString()
         }));
     }
 
-    else if(creditCard >= 4000000000000 && creditCard <= 5000000000000) {
-        var repeatI = 1000000000000;
-        var repeatJ = 10000000000000;
+    else if(creditCardNumber >= 4000000000000 && creditCardNumber < 5000000000000) {
+        repeatI = 1000000000000;
+        repeatJ = 10000000000000;
         
-        luhnsAlgorithm(creditCard, repeatI, repeatJ);
+        luhnsAlgorithm(creditCardNumber, repeatI, repeatJ);
 
         localStorage.setItem('Card', JSON.stringify({
             nameCard: 'Visa',
-            numberCard: creditCard.toString()
+            cardNumber: creditCardNumber.toString()
         }));
     }
 
-    else if(creditCard >= 4000000000000000 && creditCard <= 5000000000000000) {
-        var repeatI = 10000000000000000;
-        var repeatJ = 1000000000000000;
+    else if(creditCardNumber >= 4000000000000000 && creditCardNumber < 5000000000000000) {
+        repeatI = 10000000000000000;
+        repeatJ = 1000000000000000;
         
-        luhnsAlgorithm(creditCard, repeatI, repeatJ);
+        luhnsAlgorithm(creditCardNumber, repeatI, repeatJ);
 
         localStorage.setItem('Card', JSON.stringify({
             nameCard: 'Visa',
-            numberCard: creditCard.toString()
+            cardNumber: creditCardNumber.toString()
         }));
     }
 
     else {
         alert('Número de cartão inválido'); 
         inputElement.value = '';
-        return;
     }
 }
 
-function luhnsAlgorithm(creditCard, repeatI, repeatJ) {
-    var calculate = 0;
+function luhnsAlgorithm(creditCardNumber, repeatI, repeatJ) {
+    let calculate = 0;
 
-    for(var i = 100; i <= repeatI; i*= 100) {
-        var digits = Math.trunc((creditCard % i) / (i / 10));
-        var multiplyDigits = digits * 2;
+    for(let i = 100; i <= repeatI; i*= 100) {
+        let digits = Math.trunc((creditCardNumber % i) / (i / 10));
+        let multiplyDigits = digits * 2;
     
         if(multiplyDigits > 9) {
             calculate += multiplyDigits % 10;
@@ -98,18 +94,17 @@ function luhnsAlgorithm(creditCard, repeatI, repeatJ) {
         }
     }
 
-    for (var j = 10; j <= repeatJ; j *= 100) {
-        var digits = Math.trunc((creditCard % j) / (j / 10));
+    for (let j = 10; j <= repeatJ; j *= 100) {
+        let digits = Math.trunc((creditCardNumber % j) / (j / 10));
         calculate += digits;
     }
 
     if(calculate % 10 == 0) {
-        window.location.href = "../pages/credit-card.html"
+        window.location.href = "/pages/credit-card.html"
         inputElement.value = '';
     }
     else {
         alert('Número de cartão inválido'); 
         inputElement.value = '';
-        return;
     }
 }
