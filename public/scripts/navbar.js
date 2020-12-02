c4bbca51ce471307d9a9828ea86ec86e25b89bdf
Vinicius Gabriel <viiniciusgs03@gmail.com>
2020-12-02 14:11:58 -0300

    Navbar hamburguer

---
+++ public/scripts/navbar.js
@@ -0,0 +1,10 @@
+const hamburguer = document.querySelector('.hamburguer');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
+
+hamburguer.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+    links.forEach(link => {
+        link.classList.toggle('fade');
+    });
+});
